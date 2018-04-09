bc564eb4e43c35483b22fdcdd3833aed8ef18efb
DeepCFoxXx <learncode23@gmail.com>
2018-04-09 16:03:31 +0100

    first commit

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,9 @@
+public class LinkedList {
+
+    public Node head;
+
+    public LinkedList(String data) {
+        this.head = new Node(data);
+    }
+
+}
---
+++ src/test/java/TestLinkedList.java
@@ -0,0 +1,12 @@
+import org.junit.Before;
+
+public class TestLinkedList {
+
+    LinkedList list;
+
+    @Before
+    public void before() {
+        list = new LinkedList("head node");
+    }
+
+}
