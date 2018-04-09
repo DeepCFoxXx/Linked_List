11b85b1a4df88a9b2b0ed8a9e8c86acd72c036fa
DeepCFoxXx <learncode23@gmail.com>
2018-04-09 16:06:14 +0100

    list name passing

--- src/test/java/TestLinkedList.java
+++ src/test/java/TestLinkedList.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestLinkedList {
 
@@ -7,6 +10,11 @@ public class TestLinkedList {
     @Before
     public void before() {
         list = new LinkedList("head node");
+    }
+
+    @Test
+    public void testHeadNode() {
+        assertEquals("head node", list.head.data);
     }
 
 }
